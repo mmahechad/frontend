25d031aa44ab78f780515078393ffeb2b868bf8e
Maoe <mmahechad@hotmail.com>
2019-09-18 21:40:13 -0500

    add front

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { CountryService } from '../api/country.service';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page implements OnInit {
+paises: any [] = [];
+errorMessage: '';
+filtro: any [] = [];
+filteredProducts: any [] = [];
+
+  constructor(private countryService: CountryService) {}
+
+  ngOnInit() {
+this.countryService.obtenerPaises().subscribe(
+  paises => {
+    this.paises = paises;
+  }, error => this.errorMessage = error
+);
+console.log(this.paises);
+  }
+}
